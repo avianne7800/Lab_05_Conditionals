ed5b6dc7b4e9c1c4f62c0eaf440d8c92408e3ff3
avianne7800 <avianne7800@gmail.com>
2024-09-24 16:57:33 -0400

    More progress

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 public class ShipCostCalculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double InputPrice;
-        double InputTax;
-        double OutputPrice;
-        System.out.println("Hello world!");
+        double InputPrice = 0;
+        double InputTax = 0;
+        double OutputPrice = 0;
+        String Trash = "";
+        System.out.println("Enter the item's price: ");
+        if(in.hasNextDouble())
+        {
+            InputPrice = in.nextDouble();
+            in.nextLine();
+        }
+        else
+        {
+
+        }
     }
 }
