3203b0e5ef765874a749c4148e69b5d22d247abb
avianne7800 <avianne7800@gmail.com>
2024-09-24 16:25:50 -0400

    First commit

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ShipCostCalculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double InputPrice;
+        double InputTax;
+        double OutputPrice;
+        System.out.println("Hello world!");
+    }
+}
